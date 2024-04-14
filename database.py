c928c58f7dc1b20f1f044259469e9bad65f87ac2
YasaswiniMallem <165899453+YasaswiniMallem@users.noreply.github.com>
2024-04-14 11:50:51 +0000

    load jobs from database

---
+++ database.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine, text
+import os
+engine =  create_engine(os.environ['ENGINE'])
+
+def load_jobs_from_db():
+
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs"))
+  
+    jobs = []
+    for row in result.all():
+      jobs.append(row._asdict())
+    return jobs
+
+
+  
+
+
